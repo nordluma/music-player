5ff6bd2e67d8668c3e833e26825cb4fa9300e9b8
nordluma <maxnordlund.95@hotmail.com>
2024-11-06 20:45:25 +0200

    feat: print field name and bytes from multipart

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use axum::{
+    extract::Multipart,
     http::StatusCode,
     response::{Html, IntoResponse},
-    routing::get,
+    routing::{get, post},
     Router,
 };
 use tokio::net::TcpListener;
@@ -11,6 +12,7 @@ use tower_http::services::ServeDir;
 async fn main() -> std::io::Result<()> {
     let app = Router::new()
         .route("/", get(index))
+        .route("/upload", post(upload_file))
         .nest_service("/static", ServeDir::new("static"));
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
 
@@ -20,4 +22,12 @@ async fn main() -> std::io::Result<()> {
 async fn index() -> impl IntoResponse {
     (StatusCode::OK, Html(include_str!("../public/index.html")))
 }
+
+async fn upload_file(mut multipart: Multipart) -> impl IntoResponse {
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        let name = field.name().unwrap().to_string();
+        let bytes = field.bytes().await.unwrap();
+
+        println!("Received: {name} with bytes: {bytes:?}");
+    }
 }
